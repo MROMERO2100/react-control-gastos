cfa0a4e5b00334adeb1f34e1fcaf1e93e7cd36f4
Moises Romero <mromero2100@gmail.com>
2023-01-20 16:45:38 -0600

    Adding react-circular-progressbar, change in BudgetContro adding the CircularBaar Component.

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,23 +1,34 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css";
 import { formatearMonto } from '../helpers'
 
 const BudgetControl = ({ budget, expense }) => {
 
+    const [porcentaje, setPorcentaje] = useState(0);
     const [balance, setBalance] = useState(0);
     const [amountExpense, setAmountExpense] = useState(0);
 
     useEffect(() => {
         const totalAmountExpense = expense.reduce((total, gasto) => gasto.cantidad + total, 0)
-        const totalBalance =(budget-totalAmountExpense);
-        
+        const totalBalance = (budget - totalAmountExpense);
+
+        //PORCENTAJE GASTADO
+        const newPorcentaje = (((balance - totalBalance) / balance) * 100).toFixed(2);
+
         setAmountExpense(totalAmountExpense);
         setBalance(totalBalance);
+        setTimeout(() => {
+            setPorcentaje(newPorcentaje);
+        }, 1000);
     }, [expense])
 
     return (
         <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
             <div>
-                <p>Grafica va Aca</p>
+                <CircularProgressbar
+                    value={porcentaje}
+                />
             </div>
             <div className='contenido-presupuesto'>
                 <p><span>Presupuesto: </span> {formatearMonto(budget)}</p>
